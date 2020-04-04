6246c1f77929b8958d93bc740dd1132f4082c013
diditaditya <di2t.aditya@gmail.com>
2020-04-04 07:14:02 +0700

    refactor update by pk

--- data/table/update.go
+++ data/table/update.go
@@ -7,8 +7,8 @@ import (
 	exception "lazer/error"
 )
 
-func (table *Table) recordExistsByPk(pkColumn string, pkValue string) laze.Exception {
-	condition := fmt.Sprintf("%s = ?", pkColumn)
+func (table *Table) recordExistsByPk(pkValue string) laze.Exception {
+	condition := fmt.Sprintf("%s = ?", table.Pk)
 
 	found, err := table.Conn.Table(table.Name).Where(condition, pkValue).Rows()
 
@@ -24,32 +24,42 @@ func (table *Table) recordExistsByPk(pkColumn string, pkValue string) laze.Excep
 	return nil
 }
 
+func (table *Table) createUpdateQuery(data map[string]interface{}, params map[string][]string) (string, []interface{}) {
+	filter := table.getFilter(params)
+	whereStr, filterValues := table.createWhereStringFromFilter(filter)
 
-func (table *Table) UpdateByPk(pk string, data map[string]interface{}) laze.Exception {
-	condition := fmt.Sprintf("%s = ?", table.Pk)
-
-	err := table.recordExistsByPk(table.Pk, pk)
-	if err != nil { return err }
-	
 	values := []interface{}{}
-	columnsValues := ""
+	columnsStr := ""
 	counter := 0
 	for key, val := range data {
 		if _, ok := table.RawColumns[key]; ok {
 			if key != table.Pk {
 				values = append(values, val)
 				columnValue := fmt.Sprintf("%s = ?", key)
-				columnsValues = columnsValues + columnValue
+				columnsStr = columnsStr + columnValue
 				if counter < len(data) - 1 {
-					columnsValues = columnsValues + ", "
+					columnsStr = columnsStr + ", "
 				}
 				counter = counter + 1
 			}
 		}		
 	}
-	values = append(values, pk)
+	values = append(values, filterValues...)
 
-	query := fmt.Sprintf("UPDATE %s SET %s WHERE %s", table.Name, columnsValues, condition)
+	query := fmt.Sprintf("UPDATE %s SET %s %s", table.Name, columnsStr, whereStr)
+
+	return query, values
+}
+
+
+func (table *Table) UpdateByPk(pk string, data map[string]interface{}) laze.Exception {
+
+	err := table.recordExistsByPk(pk)
+	if err != nil { return err }
+
+	param := make(map[string][]string)
+	param[table.Pk] = []string{pk}
+	query, values := table.createUpdateQuery(data, param)
 
 	rows, updateErr := table.Conn.Raw(query, values...).Rows()
 	defer rows.Close()
