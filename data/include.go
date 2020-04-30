3b0bf5ec97b782e666b78ce8dac03bb66ea8ca4d
diditaditya <di2t.aditya@gmail.com>
2020-04-30 19:24:52 +0700

    move included struct definition to its own file

--- data/include.go
+++ data/include.go
@@ -4,52 +4,6 @@ import (
 	"lazer/data/trait"
 )
 
-type Included struct {
-	tableName				string
-	fields 					[]string
-	foreignKey			string
-	referencedField	string
-	referencedTable	string
-	referenceType		string
-	joined					[]trait.Joined
-}
-
-func (inc *Included) GetTableName() string {
-	return inc.tableName
-}
-
-func (inc *Included) GetFields() []string {
-	return inc.fields
-}
-
-func (inc *Included) SetFields(fields []string) {
-	inc.fields = fields
-}
-
-func (inc *Included) GetForeignKey() string {
-	return inc.foreignKey
-}
-
-func (inc *Included) GetReferencedField() string {
-	return inc.referencedField
-}
-
-func (inc *Included) GetReferencedTable() string {
-	return inc.referencedTable
-}
-
-func (inc *Included) GetReferenceType() string {
-	return inc.referenceType
-}
-
-func (inc *Included) GetJoined() []trait.Joined {
-	return inc.joined
-}
-
-func (inc *Included) SetJoined(joined []trait.Joined) {
-	inc.joined = joined
-}
-
 func (db *DB) isField(tableName string, field string) bool {
 	if table, tableFound := db.tables[tableName]; tableFound {
 		if _, fieldFound := table.RawColumns[field]; fieldFound {
---
+++ data/included.go
@@ -0,0 +1,51 @@
+package data
+
+import (
+	"lazer/data/trait"
+)
+
+type Included struct {
+	tableName				string
+	fields 					[]string
+	foreignKey			string
+	referencedField	string
+	referencedTable	string
+	referenceType		string
+	joined					[]trait.Joined
+}
+
+func (inc *Included) GetTableName() string {
+	return inc.tableName
+}
+
+func (inc *Included) GetFields() []string {
+	return inc.fields
+}
+
+func (inc *Included) SetFields(fields []string) {
+	inc.fields = fields
+}
+
+func (inc *Included) GetForeignKey() string {
+	return inc.foreignKey
+}
+
+func (inc *Included) GetReferencedField() string {
+	return inc.referencedField
+}
+
+func (inc *Included) GetReferencedTable() string {
+	return inc.referencedTable
+}
+
+func (inc *Included) GetReferenceType() string {
+	return inc.referenceType
+}
+
+func (inc *Included) GetJoined() []trait.Joined {
+	return inc.joined
+}
+
+func (inc *Included) SetJoined(joined []trait.Joined) {
+	inc.joined = joined
+}
